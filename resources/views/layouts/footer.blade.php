85bf41f85dacf3f982d708bffe1254b3114ba4d0
Allan Ramos <allangrds@gmail.com>
2022-05-03 11:10:36 -0300

    chore(footer): add email contact

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer">
     <p class="text">
-        Copyright © 2022 Empregos para Sabatistas. Todos os direitos reservados.
+        Copyright © 2022 Empregos para Sabatistas. Todos os direitos reservados. Para contato, envie um email para <b>contato@empregosparasabatistas.com.br</b>
     </p>
 </footer>
